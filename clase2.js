431c92a30fd852be91641264597a2277d5912a65
ferpmdev <ferpm.dev@gmail.com>
2022-10-21 20:56:06 -0300

    seguimos la 3ra clase

--- clase2.js
+++ clase2.js
@@ -42,13 +42,15 @@ function jugadaRandom() {
   let numero = parseInt(Math.random() * 3 + 1);
 
   console.log("La eleccion de la computadora es: " + numero);
+
+  return numero;
 }
 /* -------------------------------------------------------------------------- */
 /*                                  FUNCION 4                                 */
 /* -------------------------------------------------------------------------- */
 function compararJugadas() {
   const resultadosPosibles = [
-    "Genial! Ganaste! 😁",
+    "Genial! Ganaste esta ronda! 😁",
     "Esto fue un empate 😒",
     "Que lastima, perdiste 😥",
   ];
@@ -81,21 +83,77 @@ console.log(resultadoDePartida);
 // 3- A su vez debe mostrar al usuario una alerta con el resutado de la partida.
 // 4- Finalmente, si el resultado fue una derrota debe mostrarle al usuario un mensaje de aliento para desearle suerte en la próxima oportunidad.
 
-const finalPartida = (rP) => {
-  console.log(rP);
-  alert(rP);
-  if (rP === "Que lastima, perdiste 😥") {
-    alert("La próxima ganas!!!!");
+// const finalPartida = (rP) => {
+//   console.log(rP);
+//   alert(rP);
+//   if (rP === "Que lastima, perdiste 😥") {
+//     alert("La próxima ganas!!!!");
+//   }
+// };
+
+// finalPartida(resultadoDePartida);
+
+// const nuevaPartida = () => {
+//   let nuevoJuego = confirm("¿Quiere volver a jugar");
+//   if (nuevoJuego) {
+//     pedirJugada();
+//   }
+// };
+
+// nuevaPartida();
+
+/* -------------------------------------------------------------------------- */
+/*                   CONSIGNA MESA DE TRABAJO ANTERIOR                        */
+/* -------------------------------------------------------------------------- */
+// 1- Crear una función que reciba como parametro un texto (la frase de resultado de la partida).
+// 2- La función debe mostrar por consola el resultado de la partida.
+// 3- A su vez debe mostrar al usuario una alerta con el resutado de la partida.
+// 4- Finalmente, si el resultado fue una derrota debe mostrarle al usuario un mensaje de aliento para desearle suerte en la próxima oportunidad.
+
+function anunciarResultado() {
+  let puntajes = {
+    usuario: 0,
+    computadora: 0,
+    empate: 0,
+  };
+
+  while (puntajes.usuario < 3 && puntajes.computadora < 3) {
+    const resultadoPartida = compararJugadas();
+    alert(resultadoPartida);
+    console.log(resultadoPartida);
+
+    if (resultadoPartida.includes("Ganaste")) {
+      puntajes.usuario++;
+    } else if (resultadoPartida.includes("perdiste")) {
+      puntajes.computadora++;
+      alert("No te desanimes, te ira mejor en la proxima 🤗");
+    } else {
+      puntajes.empate++;
+    }
+
+    alert(
+      "Puntos del usuario: " +
+        puntajes.usuario +
+        " | Puntos de la computadora: " +
+        puntajes.computadora
+    );
+    console.log(puntajes);
   }
-};
-
-finalPartida(resultadoDePartida);
-
-const nuevaPartida = () => {
-  let nuevoJuego = confirm("¿Quiere volver a jugar");
-  if (nuevoJuego) {
-    pedirJugada();
+  if (puntajes.usuario == 3) {
+    alert("Felicitaciones!!! Fuiste el ganador de las 3 rondas");
   }
-};
+  if (puntajes.computadora == 3) {
+    alert(
+      "El ganador de las 3 rondas fue la computadora. La proxima puede ser tuya!!!!"
+    );
+  }
+}
 
-nuevaPartida();
+anunciarResultado();
+
+/* -------------------------------------------------------------------------- */
+/*                   CONSIGNA MESA DE TRABAJO (HOY)                           */
+/* -------------------------------------------------------------------------- */
+// 1- Modificar el objeto puntajes para poder contabilizar los empates tambien.
+// 2- Modificar el bucle para poder sumar tambien la cantidad de empates.
+// 3- Mostrarle al usuario quien fue el ganador final (el mejor de 3 rondas).
